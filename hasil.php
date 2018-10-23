3740e589467b094026935c59b61f7b7a01c9e1fd
ridwann17 <ridwannn17@gmail.com>
2018-10-23 09:49:28 +0700

    upload1

---
+++ hasil.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Contact V2</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+<!--===============================================================================================-->
+	<link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="css/util.css">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+<!--===============================================================================================-->
+</head>
+<body>
+
+	<div class="bg-contact2" style="background-image: url('images/bg-01.jpg');">
+		<div class="container-contact2">
+			<div class="wrap-contact2">
+				<form class="contact2-form validate-form" method="post" action="hasil.php">
+					<span class="contact2-form-title">
+						CALCULATOR BMI
+					</span>
+
+					<?php 
+					if ($_POST['hitung']) { 
+  $mass = $_POST['berat'];
+  $height = $_POST['tinggi'];
+
+  //function bmi($mass,$height) {
+    $bmi = $mass/($height*$height);
+    //return $bmi;
+  //} 
+
+  if ($bmi <= 18.5) {
+    $output = "UNDERWEIGHT";
+
+  } else if ($bmi > 18.5 AND $bmi<=24.9 ) {
+    $output = "NORMAL WEIGHT";
+
+  } else if ($bmi > 24.9 AND $bmi<=29.9) {
+    $output = "OVERWEIGHT";
+
+  } else if ($bmi > 30.0) {
+    $output = "OBESE";
+  }
+}
+echo "Your BMI value is  $bmi  and you are : "; 
+echo "$output";
+
+
+
+
+					 ?>
+					
+					
+					
+				</form>
+			</div>
+		</div>
+	</div>
+
+
+
+
+<!--===============================================================================================-->
+	<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/bootstrap/js/popper.js"></script>
+	<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/select2/select2.min.js"></script>
+<!--===============================================================================================-->
+	<script src="js/main.js"></script>
+
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-23581568-13"></script>
+	<script>
+	  window.dataLayer = window.dataLayer || [];
+	  function gtag(){dataLayer.push(arguments);}
+	  gtag('js', new Date());
+
+	  gtag('config', 'UA-23581568-13');
+	</script>
+
+</body>
+</html>
